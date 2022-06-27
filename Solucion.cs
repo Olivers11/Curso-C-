f6d83877af4ed0c7940ca3df41e34367bc36eccb
Oliver José Tzunún Dávila <olstertecn597@outlook.com>
2022-06-27 15:23:43 -0600

    Create Solucion.cs
    Solución tarea 3

---
+++ Solucion.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+
+            /*
+            -- Ejercicio 1 --
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tabla del " + N);
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "x" + N + ": " + i*N);
+            }
+            */
+            /*
+            -- Ejercicio 2 --
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tabla del " + N);
+            for (float i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "/" + N + ": " + i / N);
+            }*/
+
+            /*
+             * -- Ejercicio 3 --
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tabla del " + N);
+            for(int k = 1; k <= N; k++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i + "x" + k + ": " + i * k);
+                }
+                Console.WriteLine("----------------------------------");
+            }
+            */
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
